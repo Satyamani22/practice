ddcbaf9c5157d11c7a830446473afa3cecd3f967
Satyamani22 <151424447+Satyamani22@users.noreply.github.com>
2025-04-19 19:14:39 +0530

    Update Amar.java

--- Amar.java
+++ Amar.java
@@ -5,6 +5,7 @@ public class Amar {
 		System.out.println("Hello Amar");
       		System.out.pritln("Hello Maneesha");
 		System.out.println("Hello thara");
+		System.out.println(Hello Jathin");
 
 	}
 
